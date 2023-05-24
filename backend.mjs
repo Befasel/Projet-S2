581dbdeb32c816cf1f92f8ce341243a070949fbc
Befasel <127392157+Befasel@users.noreply.github.com>
2023-05-24 14:54:39 +0200

    pocketbase

---
+++ backend.mjs
@@ -0,0 +1,10 @@
+import PocketBase from 'pocketbase' ;
+const pb = new PocketBase ('http://127.0.0.1:8090') ;
+
+
+try { const records = await pb.collection('movies').getFullList() ;
+console.table(records);
+console.log(JSON.stringify(records, null, 2))
+} catch (e) {
+console.error(e) ;
+}
