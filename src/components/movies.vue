34ba2eda79122f6abfc7026d03cc138d11ea61cb
Befasel <127392157+Befasel@users.noreply.github.com>
2023-06-05 11:11:45 +0200

    corection

--- src/components/movies.vue
+++ src/components/movies.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { pb } from '@/backend'
 import type { MoviesResponse } from '@/pocketbase-types';
-const MoviesInfo = defineProps<MoviesResponse>();
+const MoviesInfo: MoviesResponse = defineProps<MoviesResponse>();
 const img = MoviesInfo.attach
 const urlImg = img && pb.getFileUrl(MoviesInfo, img, { thumb: '100x200' })
 </script>
@@ -9,4 +9,5 @@ const urlImg = img && pb.getFileUrl(MoviesInfo, img, { thumb: '100x200' })
     <img :src="urlImg" />
     <p>{{ title }}</p>
     <p>{{ filmer }}</p>
+    <p>{{ synopsis }}</p>
 </template>
