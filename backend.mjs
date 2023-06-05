4234ac15ad602d187b451961c50a8091e12e88de
Befasel <127392157+Befasel@users.noreply.github.com>
2023-06-05 10:47:03 +0200

    affichage des infos des films

--- backend.mjs
+++
@@ -1,9 +0,0 @@
-import PocketBase from 'pocketbase' ;
-const pb = new PocketBase ('http://127.0.0.1:8090') ;
-
-try { const records = await pb.collection('movies').getOne() ;
-console.table(records);
-console.log(JSON.stringify(records, null, 2))
-} catch (e) {
-console.error(e) ;
-} 
---
+++ src/components/movies.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { pb } from '@/backend'
+import type { MoviesResponse } from '@/pocketbase-types';
+const MoviesInfo = defineProps<MoviesResponse>();
+const img = MoviesInfo.attach
+const urlImg = img && pb.getFileUrl(MoviesInfo, img, { thumb: '100x200' })
+</script>
+<template>
+    <img :src="urlImg" />
+    <p>{{ title }}</p>
+    <p>{{ filmer }}</p>
+</template>
